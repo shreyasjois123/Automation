c4236aefd9b2149c7900525ab9dcadfcdaf3c599
DELL <DELL@jk>
2023-05-31 10:58:57 +0530

    Delete

---
+++ Automation/src/com/actitime/testscript/DeleteModule.java
@@ -0,0 +1,11 @@
+package com.actitime.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class DeleteModule {
+@Test
+public void deleteModule() {
+	Reporter.log("DeleteModule",true);
+}
+}
